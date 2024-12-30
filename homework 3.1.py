e3a83a23320e6bfad34864d9da3cf09d70bfb399
DmytroD0 <drozd.d119@gmail.com>
2024-12-30 11:28:13 +0100

    readme: just update

---
+++ homework 3.1.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter number1 "))
+diya = input("Enter diya ")
+number2 = int(input("Enter number2 "))
+if diya == "+":
+     print (number1 + number2)
+elif diya == "*":
+     print (number1 * number2)
+elif diya == "-":
+     print (number1 - number2)
+elif diya == "//" or "/":
+     print (number1 // number2)
